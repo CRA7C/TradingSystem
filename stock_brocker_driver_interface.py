a63fc4e99d071e6568c2d951aa0a9464992f2574
jong2.ha <gk1whd2@gmail.com>
2024-07-12 10:44:30 +0900

    [Interface] method 추상화

--- stock_brocker_driver_interface.py
+++ stock_brocker_driver_interface.py
@@ -1,17 +1,23 @@
-class StockerBrockerDriverInterface:
+from abc import ABC, abstractmethod
+
+
+class StockerBrockerDriverInterface(ABC):
 
     def __init__(self):
         raise TypeError
 
-
+    @abstractmethod
     def login(self, id, passwd):
         pass
 
+    @abstractmethod
     def buy(self, code, quantity, price):
         pass
 
+    @abstractmethod
     def sell(self, code, quantity, price):
         pass
 
-    def get_price(self,code):
+    @abstractmethod
+    def get_price(self, code):
         pass
