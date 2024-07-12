5bcc65feb1d6945a2a417fb511a80e0c1b8fe7a2
bj93.park <qazws9346@gmail.com>
2024-07-12 11:09:31 +0900

    [FEAT] feautures without selecting brocker

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,14 +1,18 @@
 from stock_brocker_driver_interface import StockerBrockerDriverInterface
 
+
 class AutoTradingSystem:
     def __init__(self):
+        self.NemoDriver = None
+        self.KiwerDriver = None
         self.brocker = None
 
     def select_stock_brocker(self, param):
         if param not in ['kiwer', 'nemo']:
             raise ValueError(f"{param} stock brocker does not exist.")
+        else:
+            self.brocker = None
 
-        
     def login(self, id, password):
         self.brocker.login(id, password)
 
@@ -18,8 +22,5 @@ class AutoTradingSystem:
     def sell(self, code, price, quantity):
         self.brocker.sell(code, price, quantity)
 
-    def get_price(self, code):
+    def get_price(self, code) -> int:
         return self.brocker.get_price(code)
-
-
-
