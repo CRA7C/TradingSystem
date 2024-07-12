795a94a5bb4708e6da38413c88f0f301d2767878
inpyo <winsowss@hanmail.net>
2024-07-12 11:13:50 +0900

    Use StockerBrockerDriverInterface

--- nemo_driver.py
+++ nemo_driver.py
@@ -1,17 +1,18 @@
 from nemo_api import NemoAPI
+from stock_brocker_driver_interface import StockerBrockerDriverInterface
 
 
-class NemoDriver():
+class NemoDriver(StockerBrockerDriverInterface):
     def __init__(self):
         self.__nemo_api = NemoAPI()
 
-    def login(self, id, pw):
-        self.__nemo_api.cerification(id, pw)
+    def login(self, id, passwd):
+        self.__nemo_api.cerification(id, passwd)
 
-    def buy(self, code, price, quantity):
+    def buy(self, code, quantity, price):
         self.__nemo_api.purchasing_stock(code, price, quantity)
 
-    def sell(self, code, price, quantity):
+    def sell(self, code, quantity, price):
         self.__nemo_api.selling_stock(code, price, quantity)
 
     def get_price(self, code):
