7b3db0f4b2d212dc0365fa3a60ca32e72d2353e0
inpyo <winsowss@hanmail.net>
2024-07-12 11:15:52 +0900

    Rename variable

--- nemo_driver.py
+++ nemo_driver.py
@@ -4,16 +4,16 @@ from stock_brocker_driver_interface import StockerBrockerDriverInterface
 
 class NemoDriver(StockerBrockerDriverInterface):
     def __init__(self):
-        self.__nemo_api = NemoAPI()
+        self.api = NemoAPI()
 
     def login(self, id, passwd):
-        self.__nemo_api.cerification(id, passwd)
+        self.api.cerification(id, passwd)
 
     def buy(self, code, quantity, price):
-        self.__nemo_api.purchasing_stock(code, price, quantity)
+        self.api.purchasing_stock(code, price, quantity)
 
     def sell(self, code, quantity, price):
-        self.__nemo_api.selling_stock(code, price, quantity)
+        self.api.selling_stock(code, price, quantity)
 
     def get_price(self, code):
-        self.__nemo_api.get_market_price(code, 0)
+        self.api.get_market_price(code, 0)
