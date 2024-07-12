09c2b0cde77cfca3046ccd468cbd6de4f825dc5d
geniuskey <geniuskey@gmail.com>
2024-07-12 10:46:06 +0900

    kiwer test 업데이트

--- test_kiwer_driver.py
+++ test_kiwer_driver.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import Mock, patch
+from kiwer_api import KiwerAPI
 from kiwer_driver import KiwerDriver
 
 
@@ -7,34 +8,24 @@ class TestKiwerDriver(unittest.TestCase):
 
     def setUp(self):
         self.driver = KiwerDriver()
+        self.mock_api = Mock(spec=KiwerAPI)
+        self.driver.api = self.mock_api
 
-    @patch('kiwer_driver.KiwerAPI')
-    def test_login(self, MockKiwerAPI):
-        mock_api = MockKiwerAPI()
-        self.driver.api = mock_api
+    def test_login(self):
         self.driver.login('test_id', 'test_pass')
-        mock_api.login.assert_called_once_with('test_id', 'test_pass')
+        self.mock_api.login.assert_called_once_with('test_id', 'test_pass')
 
-    @patch('kiwer_driver.KiwerAPI')
-    def test_buy(self, MockKiwerAPI):
-        mock_api = MockKiwerAPI()
-        self.driver.api = mock_api
+    def test_buy(self):
         self.driver.buy('0001', 100, 10)
-        mock_api.buy.assert_called_once_with('0001', 100, 10)
+        self.mock_api.buy.assert_called_once_with('0001', 100, 10)
 
-    @patch('kiwer_driver.KiwerAPI')
-    def test_sell(self, MockKiwerAPI):
-        mock_api = MockKiwerAPI()
-        self.driver.api = mock_api
+    def test_sell(self):
         self.driver.sell('0001', 100, 10)
-        mock_api.sell.assert_called_once_with('0001', 100, 10)
+        self.mock_api.sell.assert_called_once_with('0001', 100, 10)
 
-    @patch('kiwer_driver.KiwerAPI')
-    def test_get_price(self, MockKiwerAPI):
-        mock_api = MockKiwerAPI()
-        self.driver.api = mock_api
+    def test_get_price(self):
         self.driver.get_price('0001')
-        mock_api.current_price.assert_called_once_with('0001')
+        self.mock_api.current_price.assert_called_once_with('0001')
 
 
 if __name__ == '__main__':
