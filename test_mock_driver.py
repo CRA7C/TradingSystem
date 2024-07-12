8c4da84af42993efde014d1cdd40a3fe24d2a4a8
geniuskey <geniuskey@gmail.com>
2024-07-12 11:02:15 +0900

    mock driver 테스트 추가

---
+++ test_mock_driver.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import Mock, patch
+from mock_driver import MockDriver
+
+
+class TestMockDriver(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = MockDriver()
+        self.mock_api = Mock()
+        self.driver.api = self.mock_api
+
+    def test_login(self):
+        self.driver.login('test_id', 'test_pass')
+        self.mock_api.login.assert_called_once_with('test_id', 'test_pass')
+
+    def test_buy(self):
+        self.driver.buy('0001', 100, 10)
+        self.mock_api.buy.assert_called_once_with('0001', 100, 10)
+
+    def test_sell(self):
+        self.driver.sell('0001', 100, 10)
+        self.mock_api.sell.assert_called_once_with('0001', 100, 10)
+
+    def test_get_price(self):
+        self.driver.get_price('0001')
+        self.mock_api.current_price.assert_called_once_with('0001')
+
+
+if __name__ == '__main__':
+    unittest.main()
